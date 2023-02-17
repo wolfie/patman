6bcf956ae6765b235582c3b5ad040a9ef1b6f613
Henrik Paul <henrik.paul@reaktor.com>
2023-02-17 21:28:30 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,89 @@
+import axios, { AxiosResponse, Method as AxiosMethod } from "axios";
+
+type ServerConfig = {
+  /** e.g. https://dummy.restapiexample.com/api/v1 */
+  baseUrl: string;
+  headers?: Record<string, string>;
+};
+
+type ServiceConfigWithoutDev = { readonly prod: ServerConfig };
+type ServiceConfigWithDev = ServiceConfigWithoutDev & {
+  readonly dev: ServerConfig;
+};
+type ServiceConfig = ServiceConfigWithDev | ServiceConfigWithoutDev;
+
+type Service<HAS_DEV extends boolean> =
+  | {
+      hasDev: false;
+      config: ServiceConfigWithoutDev;
+    }
+  | ({
+      hasDev: true;
+      config: ServiceConfigWithDev;
+    } & { hasDev: HAS_DEV });
+
+type Endpoint<NAME extends string, HAS_DEV extends boolean> = {
+  readonly name: NAME;
+  callProd: () => Promise<AxiosResponse<unknown, unknown>>;
+} & (
+  | { readonly hasDev: false }
+  | {
+      readonly hasDev: true;
+      callDev: () => Promise<AxiosResponse<unknown, unknown> | undefined>;
+    }
+) & { readonly hasDev: HAS_DEV };
+
+type HasDev<T> = T extends { dev: ServerConfig } ? true : false;
+
+const createService = <T extends ServiceConfig>(
+  serviceConfig: T
+): Service<HasDev<T>> => {
+  return {
+    hasDev: ("dev" in serviceConfig) as any,
+    config: serviceConfig,
+  };
+};
+
+const createCall =
+  (method: AxiosMethod, path: string, config: ServerConfig) => () =>
+    axios.request({
+      method,
+      url: `${config.baseUrl}/${path}`,
+      headers: config.headers,
+    });
+
+const serviceHasDev = <SERVICE extends Service<any>>(
+  service: SERVICE
+): service is SERVICE & { hasDev: true } => service.hasDev;
+
+const createEndpoint = <NAME extends string, HAS_DEV extends boolean>(
+  service: Service<HAS_DEV>,
+  name: NAME,
+  method: AxiosMethod,
+  path: string
+): Endpoint<NAME, HAS_DEV> => {
+  if (serviceHasDev(service)) {
+    return {
+      name,
+      hasDev: service.hasDev,
+      callProd: createCall(method, path, service.config.prod),
+      callDev: createCall(method, path, service.config.dev),
+    };
+  } else {
+    return {
+      name,
+      hasDev: service.hasDev as false & HAS_DEV,
+      callProd: createCall(method, path, service.config.prod),
+    };
+  }
+};
+
+const service = createService({
+  hasDev: false,
+  prod: { baseUrl: "https://dummy.restapiexample.com/api/v1" },
+  dev: { baseUrl: "https://dummy.restapiexample.com/api/v1" },
+});
+
+const endpoint = createEndpoint(service, "employees", "GET", "/employees");
+
+endpoint.callProd().then((response) => console.log(response.data));
