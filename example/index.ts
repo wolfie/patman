d4efe4fd5cf74a4debc1d87814700777a27d605f
Henrik Paul <henrik.paul@reaktor.com>
2023-02-19 20:43:03 +0200

    extract example into a file of its own

---
+++ example/index.ts
@@ -0,0 +1,61 @@
+import Patman, { Endpoint, IotsParseError, ServiceEnv } from '../src';
+import * as t from 'io-ts';
+import { isAxiosError } from 'axios';
+
+const MeatAndFillerArgs = t.intersection([
+  t.type({
+    type: t.union([t.literal('meat-and-filler'), t.literal('all-meat')]),
+  }),
+  t.partial({
+    paras: t.number,
+    startWithLorem: t.literal(1),
+    format: t.union([t.literal('json'), t.literal('text'), t.literal('html')]),
+  }),
+]);
+
+const patman = new Patman(
+  { prod: { baseUrl: 'https://baconipsum.com/api' } },
+  {
+    meatAndFiller: {
+      method: 'GET',
+      args: MeatAndFillerArgs,
+      path: () => `/`,
+      params: (args) => args,
+      body: t.array(t.string),
+    } satisfies Endpoint<typeof MeatAndFillerArgs, any>,
+    static: {
+      method: 'GET',
+      path: '/',
+      params: { type: 'meat-and-filler' },
+      body: t.array(t.string),
+    },
+  }
+);
+
+const handleIotsParseError =
+  <T>(fallbackValue: T) =>
+  (e: unknown): T => {
+    if (e instanceof IotsParseError) return fallbackValue;
+    else throw e;
+  };
+
+const handleAxios404Response =
+  <T>(fallbackValue: T) =>
+  (e: unknown) => {
+    if (isAxiosError(e) && e.response?.status === 404) {
+      return fallbackValue;
+    } else throw e;
+  };
+
+(async () => {
+  const result1 = patman
+    .$({ prod: 'meatAndFiller' })({ type: 'all-meat', paras: 1 })
+    .then((response) => response.data)
+    .catch(handleIotsParseError<string[]>([]));
+
+  const result2 = patman
+    .$({ prod: 'static' })()
+    .then((response) => response.data)
+    .catch(handleIotsParseError<string[]>([]))
+    .catch(handleAxios404Response<string[]>([]));
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-import axios, { Method as AxiosMethod, type AxiosResponse, type AxiosRequestConfig } from 'axios';
+import axios, {
+  Method as AxiosMethod,
+  type AxiosResponse,
+  type AxiosRequestConfig,
+  AxiosError,
+} from 'axios';
 import * as t from 'io-ts';
 import { stringify } from 'qs';
+import debug from 'debug';
+
+let reqId = 0;
+type AxiosRequestConfigWithCorrelationId = AxiosRequestConfig & { reqId: number };
+const hasReqId = <T>(config: T): config is T & { reqId: number } =>
+  typeof (config as any)['reqId'] === 'number';
+
+const logger = debug('patman');
+const requestLogger = logger.extend('request');
+const responseLogger = logger.extend('response');
+
+const removeUndefinedProps = <T>(t: Record<string, T | undefined>): Record<string, T> =>
+  Object.fromEntries(Object.entries(t).filter(([_, value]) => typeof value !== 'undefined')) as any;
 
 const tap =
   <T>(tapper: (t: T) => void) =>
@@ -9,17 +27,56 @@ const tap =
     return t;
   };
 
-const LOG = false as boolean;
+if (requestLogger.enabled)
+  axios.interceptors.request.use(
+    tap((request) =>
+      requestLogger(hasReqId(request) ? request.reqId : '###', '[headers]:', request.headers)
+    )
+  );
 
-const logger = LOG ? (...args: any[]) => console.log(...args) : () => {};
-if (LOG) axios.interceptors.request.use(tap((request) => logger({ headers: request.headers })));
+const cropStringEnd = (maxLength: number) => (string: string) =>
+  string.length > maxLength ? string.substring(0, maxLength) + '…' : string;
+const cropStringStart = (maxLength: number) => (string: string) =>
+  string.length > maxLength ? '…' + string.substring(maxLength) : string;
+export class IotsParseError extends Error {
+  public formattedErrors;
+  constructor(public originalErrors: t.Errors) {
+    const formattedErrors = formatIotsError(originalErrors);
+    const errorMsg = JSON.stringify(
+      formattedErrors.map(({ expectedType, path, ...error }) => ({
+        ...error,
+        path: cropStringStart(50)(path),
+        expectedType: cropStringEnd(50)(expectedType),
+        originalValue:
+          'originalValue' in error ? cropStringEnd(50)((error as any).originalValue) : undefined,
+      })),
+      null,
+      2
+    );
+    super(errorMsg.length > 200 ? errorMsg.substring(0, 200) + '…' : errorMsg);
+    this.formattedErrors = formattedErrors;
+  }
+}
+
+const formatIotsError = (errors: t.Errors) =>
+  errors.map((error) => {
+    const base = {
+      path: error.context.map((c) => c.key || '#root').join('.'),
+      expectedType: error.context[error.context.length - 1].type.name,
+    };
+    const parsedError =
+      typeof error.value === 'undefined'
+        ? { ...base, originalValueIsUndefined: true }
+        : { ...base, originalValue: error.value };
+    return parsedError;
+  });
 
 const decode =
   <T extends t.Any>(t: T) =>
   (x: unknown): t.TypeOf<T> => {
     const result = t.decode(x);
     if (result._tag === 'Right') return result.right;
-    else throw new Error(JSON.stringify(result.left));
+    else throw new IotsParseError(result.left);
   };
 
 const AxiosResponse = <T extends t.Any>(data: T) =>
@@ -31,8 +88,8 @@ const AxiosResponse = <T extends t.Any>(data: T) =>
     config: t.UnknownRecord,
   });
 
-type ParamValues = string | number | boolean;
-type Endpoint<ARGS extends t.Any, BODY extends t.Any> = {
+export type ParamValues = string | number | boolean;
+export type Endpoint<ARGS extends t.Any, BODY extends t.Any> = {
   method: AxiosMethod;
   body?: BODY;
 } & (
@@ -52,7 +109,7 @@ type Endpoint<ARGS extends t.Any, BODY extends t.Any> = {
     }
 );
 
-type ServiceEnv = {
+export type ServiceEnv = {
   baseUrl: `${'http' | 'https'}://${string}`;
   headers?: Record<string, string>;
 };
@@ -63,28 +120,40 @@ class Patman<SNAMES extends string, E extends Record<string, Endpoint<any, any>>
   private fnFromServiceandEndpoint =
     <S extends ServiceEnv, E extends Endpoint<any, any>>(s: S, e: E) =>
     (args: t.TypeOf<E['args']>): Promise<AxiosResponse<t.TypeOf<E['body']>>> => {
-      const axiosOptions: AxiosRequestConfig = {
+      const axiosOptions: AxiosRequestConfigWithCorrelationId = {
         method: e.method,
         url: `${s.baseUrl}${typeof e.path === 'string' ? e.path : e.path(args)}`,
         headers: { ...s.headers, ...e.headers },
-        params: typeof e.params === 'function' ? e.params(args) : e,
-        paramsSerializer: { serialize: (params) => stringify(params, { arrayFormat: 'repeat' }) },
+        params: typeof e.params === 'function' ? e.params(args) : e.params,
+        paramsSerializer: {
+          serialize: (params) => stringify(removeUndefinedProps(params), { arrayFormat: 'repeat' }),
+        },
+        reqId: reqId++,
       };
 
-      logger(JSON.stringify({ axiosOptions, uri: axios.getUri(axiosOptions) }, null, 2));
+      requestLogger(axiosOptions.reqId, e.method, axios.getUri(axiosOptions));
 
       const promise = axios.request(axiosOptions).then(decode(AxiosResponse(e.body ?? t.unknown)));
-      if (LOG) {
-        promise
-          .then((response) =>
-            logger({
-              status: `${response.status} ${response.statusText}`,
-              headers: response.headers,
-            })
-          )
-          .catch(logger);
-      }
-      return promise as any;
+      return responseLogger.enabled
+        ? (promise
+            .then(
+              tap((response) => {
+                responseLogger(
+                  axiosOptions.reqId,
+                  '[status]:',
+                  `${response.status} ${response.statusText}`
+                );
+                responseLogger(axiosOptions.reqId, '[headers]:', response.headers);
+              })
+            )
+            .catch((e) => {
+              if (e instanceof AxiosError)
+                responseLogger(axiosOptions.reqId, 'AxiosError: ' + e.message);
+              else if (e instanceof IotsParseError)
+                responseLogger(axiosOptions.reqId, 'IotsParseError: ' + e.message);
+              throw e;
+            }) as any)
+        : promise;
     };
 
   $ = <SNAME extends SNAMES, ENAME extends keyof E>(path: Record<SNAME, ENAME>) => {
@@ -95,46 +164,4 @@ class Patman<SNAMES extends string, E extends Record<string, Endpoint<any, any>>
   };
 }
 
-const MeatAndFillerArgs = t.intersection([
-  t.type({
-    type: t.union([t.literal('meat-and-filler'), t.literal('all-meat')]),
-  }),
-  t.partial({
-    paras: t.number,
-    startWithLorem: t.literal(1),
-    format: t.union([t.literal('json'), t.literal('text'), t.literal('html')]),
-  }),
-]);
-
-const patman = new Patman(
-  {
-    prod: {
-      baseUrl: 'https://baconipsum.com/api',
-    } satisfies ServiceEnv,
-  },
-  {
-    static: {
-      method: 'GET',
-      path: '/',
-      params: { type: 'meat-and-filler' },
-      body: t.array(t.string),
-    },
-    meatAndFiller: {
-      method: 'GET',
-      args: MeatAndFillerArgs,
-      path: () => `/`,
-      params: (args) => args,
-      body: t.array(t.string),
-    } satisfies Endpoint<typeof MeatAndFillerArgs, any>,
-  }
-);
-
-patman
-  .$({ prod: 'meatAndFiller' })({ type: 'all-meat', paras: 1 })
-  .then((response) => response.data)
-  .then(console.log);
-
-patman
-  .$({ prod: 'static' })()
-  .then((response) => response.data)
-  .then(console.log);
+export default Patman;
